f41e4a77629520f84bd4fa0664d16c20c05cc132
Mario Tema <mario.tema@unic.com>
2018-08-01 22:19:48 +0200

    Create Maven Project

---
+++ src/main/java/de/seven/fate/simpletexteditorserver/SimpleTextEditorServerApplication.java
@@ -0,0 +1,12 @@
+package de.seven.fate.simpletexteditorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTextEditorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTextEditorServerApplication.class, args);
+    }
+}
